bac70969537a96b8a2cb1e5b3deb2ce053a9c716
Harikrishnan Anil <harikrishnan.anilkumar@ust.com>
2021-09-17 11:53:10 +0530

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import{ DashComponent} from './dash/dash.component';
+import {AdminloginComponent} from './adminlogin/adminlogin.component';
+import {AuthGuard} from './authentication/auth.guard';
+import {EmployeeloginComponent} from './employeelogin/employeelogin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:DashComponent},
+  {path:"adminlogin",component:AdminloginComponent},
+  {path:"employeelogin",component:EmployeeloginComponent},
+  {path:"admin",loadChildren:()=>import('./admin/admin.module').then(m=>m.AdminModule),canActivate:[AuthGuard]},
+  {path:"employee",loadChildren:()=>import('./employee/employee.module').then(m=>m.EmployeeModule),canActivate:[AuthGuard]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
